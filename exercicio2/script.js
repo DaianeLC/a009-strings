11809879208b2dc82f6f509a86a57daa90799a9f
Daiane <daianelopescorrea1@gmail.com>
2022-07-26 20:28:46 -0300

    realização de todos os exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim())
+console.log(minhaString.length,minhaString.trim())
+
+const novaFrase = minhaString.replaceAll("________","sticioso")
+console.log(novaFrase)
+
+const semEspaco = minhaString.trim()
+console.log(semEspaco.length)
+
+
+//B- preciso de mais opções que fiquem bonitas
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair"
+const novaFrase = frase.replace("verde","rosa").replace("Azul","Laranja")// aqui vc pode criar um ponto replace
+
+const includeNovaFra = frase.includes("verde","laranja")
+const extra = " mas não deixe o gato sair\""
+
+console.log(frase)
+console.log(novaFrase)
+console.log(includeNovaFra)
+
+const fraseExtra =`Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDA${extra.toLocaleUpperCase()}`
+
+console.log(fraseExtra)
+
+// consigo usar dois replace na mesma frase, mais não consigo colocar dois include. Para fazer o exercicio EXTRA criei uma variavel para a frase solicitada e pedindo para ficar em maiusculo
